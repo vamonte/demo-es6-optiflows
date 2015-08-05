0e04d299e9f1c88656409977377548afb28baf30
Guillaume Desvé <guillaume.desve@surycat.com>
2015-08-05 14:52:31 +0200

    docker-compose, sentence fix

--- src/app1/tibo.js
+++ src/app1/tibo.js
@@ -1,37 +1,47 @@
 import Developer from "base/developer.js";
 
 class Tibo extends Developer {
-    
-    constructor(name){
+
+    constructor(name) {
         super(name);
         this.is_intern = true;
         this.said_id = [];
     }
 
-    get bullshit(){
-        return ['Encore des wesh wesh avec leurs skates', "J'aime pas le soleil, c'est chaud.",
-                "On lui a parlé à lui?", "Nique ta maman", "Marie-Cyril!!!!", "J'aime les enfants.",
-                "J'ai passé mon week end dans une cave."];
+    get bullshit() {
+        return [
+            "Encore des wesh wesh avec leurs skates",
+            "J'aime pas le soleil, c'est chaud.",
+            "On lui a parlé à lui?", "Nique ta maman",
+            "Marie-Cyril!!!!",
+            "J'aime les enfants.",
+            "J'ai passé mon week end dans une cave."];
     }
-    addSaidId(_id){
+
+    addSaidId(_id) {
         this.said_id.push(_id);
         if(this.said_id.length === this.bullshit.length){
             this.said_id = [];
         }
     }
 
-    getRandomBullshit(){
+    getRandomBullshit() {
         var random_id = -1;
-        do{
+        do {
             random_id = parseInt(Math.random() * this.bullshit.length);
-        }while(this.said_id.indexOf(random_id) > -1);
+        } while(this.said_id.indexOf(random_id) > -1);
 
         this.addSaidId(random_id);
 
         return this.bullshit[random_id];
     }
-    say(sentence){
-        var sentence = this.getRandomBullshit();
+
+    say(sentence) {
+        if (sentence.indexOf("windows") > -1) {
+            var sentence = "C'est de la merde";
+        } else {
+            var sentence = this.getRandomBullshit();
+        }
         return super.say(sentence);
     }
 }
